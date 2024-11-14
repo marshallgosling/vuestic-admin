aa04393ee2ab36fae09c31089fdbe6682384998c
migo <marshall.goslin@icloud.com>
2024-11-14 10:43:59 +0800

    html title

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-import VuesticLogo from '../components/VuesticLogo.vue'
+import CenturyLogo from '../components/CenturyLogo.vue'
 import NotFoundImage from '../components/NotFoundImage.vue'
 </script>
 
 <template>
   <div class="flex flex-col justify-between h-screen items-center bg-[var(--va-background-secondary)]">
     <RouterLink to="/">
-      <VuesticLogo :gradient="false" class="my-8 h-5" />
+      <CenturyLogo :gradient="false" class="my-8 h-5" />
     </RouterLink>
 
     <div class="flex flex-col items-center gap-6 px-4 my-8">
@@ -19,9 +19,6 @@ import NotFoundImage from '../components/NotFoundImage.vue'
 
       <div class="flex flex-col sm:flex-row gap-4">
         <VaButton to="/">Go to homepage</VaButton>
-        <VaButton href="https://github.com/epicmaxco/vuestic-admin/issues/new" preset="secondary" target="_blank"
-          >Create a GitHub issue
-        </VaButton>
       </div>
     </div>
 
